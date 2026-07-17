056b5a2b8509fa71dc5fb65d5c1de70e9df354ab
dqhuy0604 <2251120015@ut.edu.vn>
2026-07-17 16:04:29 +0700

    backend- tao chuc nang dang nhap

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./../models/user');
 
 router.post('/signup' ,async (req , res) =>{
@@ -29,5 +30,38 @@ router.post('/signup' ,async (req , res) =>{
         });
     }
 });
+
+router.post('/login', async (req , res) => {
+    try{
+        const user = await User.findOne({email : req.body.email});
+        if(!user){
+            return res.send({
+                message : 'User dose not exist',
+                succes : false
+            })
+        }
+            const isvalid = await bcrypt.compare(req.body.password , user.password);
+            if(!isvalid){
+             return res.send({
+                message : 'invalid password',
+                succes : false
+            })
+        }
+        const token = jwt.sign({userId : user._id}, process.env.SECRET_KEY, {expiresIn :"1d"}); 
+
+        res.send({
+                message : 'user logged-in successfully', 
+                succes : true, 
+                token : token
+        });
+
+
+    }catch(error){
+        res.send({
+            message : error.message,
+            succes : false
+        })
+    }
+})
 
 module.exports = router; 
