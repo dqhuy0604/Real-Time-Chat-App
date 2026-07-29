52ef76d90341aa9c96e62c66ffe3c226c30e7876
dqhuy0604 <2251120015@ut.edu.vn>
2026-07-30 02:58:10 +0700

    tao api call , components , xac thuc token cua tai khoan , lay thong tin cua user thong qua token

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/home';
 import Login from './pages/login' ;
 import Signup from './pages/signup';
 import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./components/protectedRoute";
 
 
 function App() {
@@ -11,7 +12,11 @@ function App() {
       <Toaster position="top-center" reverseOrder={false}/>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home /> }> </Route>
+          <Route path="/" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>}>
+          </Route>
           <Route path="/login" element={<Login /> }> </Route>
           <Route path="/signup" element={<Signup /> }> </Route>
         </Routes>
---
+++ client/src/components/protectedRoute.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getLoggedUser } from "../apiCalls/users";
+
+function ProtectedRoute({children}){
+    const [user , setUser] = useState(null);
+
+    const navigate = useNavigate();
+
+    const getloggedInUser = async () =>{
+
+        let response = null; 
+        try{
+            response = await getLoggedUser();
+            if(response.success){
+                setUser(response.data)
+            }else{
+                navigate('/login');
+            }
+        }catch(error){
+             navigate('/login');
+        }
+    } 
+    useEffect(() => {
+        if(localStorage.getItem('token')){
+            getloggedInUser();
+        }else{
+            navigate('/login');
+        }
+    });
+    
+    return(
+    <div>
+        <p>Name : { user?.firstname + ' ' + user?.lastname }</p>
+        <p>Email :{ user?.email} </p>
+        {children}
+    </div>
+    )
+}
+
+export default ProtectedRoute;
