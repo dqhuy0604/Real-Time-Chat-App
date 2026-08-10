492af5362fd608b77818953cd462eda912003f5a
dqhuy0604 <2251120015@ut.edu.vn>
2026-08-11 01:33:19 +0700

    thay doi URL de render

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:5000";
+export const url = "https://zolaapp.onrender.com";
 
 export const axiosInstance = axios.create({
     headers: {
